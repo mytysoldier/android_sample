9a7d565e570fed7964f4ab56503cdaeb9e175faa
mytysoldier <yoshi008t@gmail.com>
2023-11-18 19:10:59 +0900

    sample

--- android_sample/HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
+++ android_sample/HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.hellosample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btClick = findViewById<Button>(R.id.btClick)
+        val listener = HelloListener()
+        btClick.setOnClickListener(listener)
+
+        val btClear = findViewById<Button>(R.id.btClear)
+        btClear.setOnClickListener(listener)
+    }
+
+    private inner class HelloListener : View.OnClickListener {
+
+        override fun onClick(v: View?) {
+            val input = findViewById<EditText>(R.id.dtName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            when(v?.id) {
+                R.id.btClick -> {
+                    val inputStr = input.text.toString()
+                    output.text = inputStr + "さん、こんにちは！"
+                }
+                R.id.btClear -> {
+                    input.setText("")
+                    output.text = ""
+                }
+            }
+        }
     }
 }
