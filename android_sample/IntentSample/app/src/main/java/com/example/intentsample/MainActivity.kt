ef9e77258d5c866f92d96fafd659e846a910cfaf
mytysoldier <yoshi008t@gmail.com>
2023-12-02 19:28:21 +0900

    sample

--- android_sample/IntentSample/app/src/main/java/com/example/intentsample/MainActivity.kt
+++ android_sample/IntentSample/app/src/main/java/com/example/intentsample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.intentsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 
@@ -24,5 +27,23 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = SimpleAdapter(this@MainActivity, menuList, android.R.layout.simple_list_item_2, from, to)
         lvMenu.adapter = adapter
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            val item = parent?.getItemAtPosition(position) as MutableMap<String, String>
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+//            val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
+//            intent2MenuThanks.putExtra("menuName", menuName)
+//            intent2MenuThanks.putExtra("menuPrice", menuPrice)
+            val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java).apply {
+                putExtra("menuName", menuName)
+                putExtra("menuPrice", menuPrice)
+            }
+
+            startActivity(intent2MenuThanks)
+        }
     }
 }
